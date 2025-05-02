7d7ee0be0c9e5fdea6e9216be7da3e6101c1f24a
Greg <gmerrill03@gmail.com>
2025-05-02 10:01:15 -0700

    finish milestone 1

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,24 @@
+// JavaScript for Opening and Closing the Modal
+var modal = document.getElementById('playlistModal')
+var span = document.getElementsByClassName('close')[0]
 
+function openModal(playlist) {
+  document.getElementById('playlistImage').src = playlist.imageUrl
+  document.getElementById('playlistTitle').innerText = playlist.title
+  document.getElementById(
+    'playlistAuthor',
+  ).innerText = `Author: ${playlist.author}`
+  document.getElementById(
+    'playlistSongs',
+  ).innerHTML = `<strong>Songs:</strong> ${playlist.songs.join(', ')}`
+  modal.style.display = 'block'
+}
+
+span.onclick = function () {
+  modal.style.display = 'none'
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none'
+  }
+}
